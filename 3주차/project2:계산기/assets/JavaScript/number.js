29fe6d099484eaff3220f1e9fb4fb29ad4c97a20
namyoonseo <ysnam0123@naver.com>
2024-12-27 23:48:41 +0900

    commit

--- 3주차/project2:계산기/assets/JavaScript/number.js
+++ 3주차/project2:계산기/assets/JavaScript/number.js
@@ -1,36 +1,36 @@
 const numberButtons = document.querySelectorAll('.number');
-const resultScreen = document.querySelector('.result'); // 화면에 표시할 요소
-let result = ''; // 계산식 저장 변수
+const resultScreen = document.querySelector('.result');
+let result = '';
 
 const offButton = document.querySelector('.offbutton');
 const onButton = document.querySelector('.onbutton');
 const clearButton = document.querySelector('.clear');
 
-// OFF 버튼 클릭 이벤트
+// OFF
 offButton.onclick = function () {
   resultScreen.style.color = '#d8d2c2'; // 화면 텍스트 색상 변경
 };
 
-// ON 버튼 클릭 이벤트
+// ON
 onButton.onclick = function () {
-  resultScreen.style.color = 'black'; // 텍스트 색상 복원
-  result = ''; // 계산식 초기화
-  resultScreen.textContent = '0'; // 화면에 "0" 표시
+  resultScreen.style.color = 'black';
+  result = '';
+  resultScreen.textContent = '0';
 };
 
-// 숫자 버튼 클릭 이벤트
+// 숫자 버튼
 numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    const number = button.textContent; // 버튼의 텍스트 값 가져오기
+    const number = button.textContent;
     result += number; // 계산식 문자열에 숫자 추가
-    resultScreen.textContent = result; // 화면에 계산식 표시
+    resultScreen.textContent = result;
   });
 });
 
-// CLEAR 버튼 클릭 이벤트
+// CLEAR 버튼
 clearButton.addEventListener('click', () => {
-  result = ''; // 계산식 초기화
-  resultScreen.textContent = '0'; // 화면 초기화
+  result = '';
+  resultScreen.textContent = '0';
 });
 
 // plus
